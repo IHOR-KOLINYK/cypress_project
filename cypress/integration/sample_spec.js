f2287aa858c11c636449cb40258ffaa0d4830080
unknown <kolinyk.ihor@nielsen.com>
2019-11-12 10:47:18 +0200

    assert added

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -9,21 +9,21 @@ describe('Test suite', () => {
     it('first test', () => {
         cy.xpath("//*[@class='gLFyf gsfi']").type("first test");
         cy.xpath("//*[@class='gNO89b']").click();
-        expect(cy.xpath("//h3[text()='Test F.I.R.S.T - By - Hacker Noon']")).should('exist');
+        assert.exist(cy.xpath("//h3[text()='Test F.I.R.S.T - By - Hacker Noon']"),"element not exist");
     });
 
 
     it('second test', () => {
         cy.xpath("//*[@class='gLFyf gsfi']").type("second test");
         cy.xpath("//*[@class='gNO89b']").click();
-        expect(cy.xpath("//h3[text()='Test F.I.R.S.T - By - Hacker Noon']")).should('exist');
+        assert.exist(cy.xpath("//h3[text()='Test F.I.R.S.T - By - Hacker Noon']"),"element not exist");
     });
 
 
     it('third test', () => {
         cy.xpath("//*[@class='gLFyf gsfi']").type("third test");
         cy.xpath("//*[@class='gNO89b']").click();
-        expect(cy.xpath("//h3[text()='Test F.I.R.S.T - By - Hacker Noon']")).should('exist');
+        assert.exist(cy.xpath("//h3[text()='Test F.I.R.S.T - By - Hacker Noon']"),"element not exist");
     });
 
 
